8a862c4bb1c106e8721a6855fe0aa6a4cfe59dce
Simon Stewart <simon.james.stewart@gmail.com>
2024-08-15 20:11:48 +0100

    commt

--- src/assets/js/offline-events.js
+++ src/assets/js/offline-events.js
@@ -1,1 +1,20 @@
-// This file is now empty because offline notifications and connectivity checks are not needed.
+import Toastify from 'toastify-js';
+
+let isOffline = false;
+const bodyElement = document.querySelector('body');
+
+function updateConnectivityStatus() {
+	let notificationToShow = false;
+	let notificationClass = '';
+	let notificationText = '';
+	let notificationDuration = 3000;
+	let notificationDestination = false;
+}
+
+function checkConnectivity() {
+	window.addEventListener('online', updateConnectivityStatus);
+	window.addEventListener('offline', updateConnectivityStatus);
+	updateConnectivityStatus();
+}
+
+window.addEventListener('load', checkConnectivity);
