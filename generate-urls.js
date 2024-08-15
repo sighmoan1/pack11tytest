bf75572abb9c8bd904a34348232dbc078994a00f
Simon Stewart <simon.james.stewart@gmail.com>
2024-08-15 19:59:40 +0100

    precache and remove toastify

--- generate-urls.js
+++ generate-urls.js
@@ -2,23 +2,30 @@ const path = require('path');
 const fs = require('fs');
 
 function generateListOfURLs(dir, baseurl) {
-  let urls = [];
-  const files = fs.readdirSync(dir);
+	let urls = [];
+	try {
+		const files = fs.readdirSync(dir);
 
-  files.forEach(file => {
-    const absolutePath = path.join(dir, file);
-    if (fs.statSync(absolutePath).isDirectory()) {
-      urls = [...urls, ...generateListOfURLs(absolutePath, baseurl + file + '/')];
-    } else if (file === 'index.html') {
-      urls.push(baseurl);
-    }
-  });
+		files.forEach((file) => {
+			const absolutePath = path.join(dir, file);
+			if (fs.statSync(absolutePath).isDirectory()) {
+				urls = [
+					...urls,
+					...generateListOfURLs(absolutePath, baseurl + file + '/'),
+				];
+			} else if (file === 'index.html') {
+				urls.push(baseurl);
+			}
+		});
+	} catch (error) {
+		console.error(`Error reading directory ${dir}:`, error);
+	}
 
-  return urls;
+	return urls;
 }
 
 const listOfURLs = generateListOfURLs('_site', '/');
-// Use 'listOfURLs' in your service worker 'warmCache' option
+console.log('URLs to be cached:', listOfURLs.join(', '));
 
 const serviceWorkerContent = `
 import {
@@ -28,29 +35,24 @@ import {
 	imageCache,
 } from 'workbox-recipes';
 
-// Enable navigation preload
 import { enable } from 'workbox-navigation-preload';
 enable();
 
-// Serve pages as network first, with 2 seconds timeout and cache fallback
 pageCache({
 	networkTimoutSeconds: 2,
-	warmCache: ['/', '/documentation/', '/offline/', '/emergencies','/images/', '/lifecycle', ${listOfURLs.map(url => `'${url}'`).join(', ')}],
+	warmCache: ['/', '/documentation/', '/offline/', '/emergencies','/images/', '/lifecycle', ${listOfURLs.map((url) => `'${url}'`).join(', ')}],
 });
 
-// Serve static assets from immediately from cache, and update (aka "Stale While Revalidate")
 staticResourceCache({
 	warmCache: self.__WB_MANIFEST,
 });
 
-// Cache a maximum of 100 images, for 90 days each at most
 imageCache({
 	maxEntries: 100,
 	maxAgeSeconds: 60 * 60 * 24 * 90,
 	warmCache: ['./images/logo-192px.png', './images/logo-512px.png'],
 });
 
-// Provide offline fallbacks for HTML and images
 offlineFallback({
 	pageFallback: '/offline/fallback.html',
 	imageFallback: '/offline/fallback.svg',
--- workbox.config.js
+++ workbox.config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	globDirectory: '_site',
-	globPatterns: ['./bundle/*', './manifest.webmanifest', './*'],
+	globPatterns: ['**/*.{html,js,css,png,jpg,jpeg,svg,gif,woff2}'],
 	swSrc: '_site/service-worker.js',
 	swDest: '_site/service-worker.js',
 };
